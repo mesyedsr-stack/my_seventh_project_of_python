d9a19de88aab3bae16554e2ee51636c77b2b8814
Syed Sameer <mesyedsr@gmail.com>
2026-07-15 22:18:12 +0530

    Add files via upload

---
+++ sam12.py
@@ -0,0 +1,117 @@
+#tables
+#from 1 to 20
+print('2 table:-')
+
+for s in range(1,11,1):
+	print("2*",s,"=",2*s)
+print('')	
+	
+print('3 table:-')
+
+for s in range(1,11,1):
+	print("3*",s,"=",3*s)
+print('')	
+	
+print('4 table:-')
+
+for s in range(1,11,1):
+	print("4*",s,"=",4*s)
+print('')	
+	
+print('5 table:-')
+
+for s in range(1,11,1):
+	print("5*",s,"=",5*s)
+print('')	
+	
+print('6 table:-')
+
+for s in range(1,11,1):
+	print("6*",s,"=",6*s)
+print('')	
+	
+print('7 table:-')
+
+for s in range(1,11,1):
+	print("7*",s,"=",7*s)
+print('')	
+	
+print('8 table:-')
+
+for s in range(1,11,1):
+	print("8*",s,"=",8*s)
+print('')	
+	
+print('9 table:-')	
+
+for s in range(1,11,1):
+	print("9*",s,"=",9*s)
+print('')	
+	
+print('10 table:-')
+
+for s in range(1,11,1):
+	print("10*",s,"=",10*s)
+print('')	
+	
+print('11 table:-')
+
+for s in range(1,11,1):
+	print("11*",s,"=",11*s)
+print('')	
+	
+print('12 table:-')
+
+for s in range(1,11,1):
+	print("12*",s,"=",12*s)
+print('')	
+	
+print('13 table:-')	
+
+for s in range(1,11,1):
+	print("13*",s,"=",13*s)
+print('')	
+	
+print('14 table:-')	
+
+for s in range(1,11,1):
+	print("14*",s,"=",14*s)
+print('')	
+	
+print('15 table:-')	
+
+for s in range(1,11,1):
+	print("15*",s,"=",15*s)
+print('')	
+	
+print('16 table:-')
+
+for s in range(1,11,1):
+	print("16*",s,"=",16*s)
+print('')	
+	
+print('17 table:-')	
+
+for s in range(1,11,1):
+	print("17*",s,"=",17*s)
+print('')	
+	
+print('18 table:-')	
+
+for s in range(1,11,1):
+	print("18*",s,"=",18*s)
+print('')	
+	
+print('19 table:-')
+
+for s in range(1,11,1):
+	print("19*",s,"=",19*s)
+print('')	
+	
+print('20 table:-')	
+
+for s in range(1,11,1):
+	print("20*",s,"=",20*s)
+print('')	
+	
+
